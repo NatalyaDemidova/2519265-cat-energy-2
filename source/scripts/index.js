85c4fec342a59128dcb88aeb20d6a6ab0e388d90
= <demidovanatalia691991@gmail.com>
2024-12-19 17:25:24 +0300

    module7-task1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,20 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.nav-menu');
+const navToggle = document.querySelector('.button__menu');
+
+navMain.classList.remove('nav-menu--nojs');
+navToggle.classList.remove('button__menu--nojs');
+
+// navToggle.addEventListener('click', () => {
+//   if (navMain.classList.contains('nav-menu--closed')) {
+//     navMain.classList.remove('nav-menu--closed');
+//     navToggle.classList.remove('button__menu--closed');
+//     navMain.classList.add('nav-menu--open');
+//     navToggle.classList.add('button__menu--open');
+//   } else {
+//     navMain.classList.add('nav-menu--closed');
+//     navToggle.classList.add('button__menu--closed');
+//     navMain.classList.remove('nav-menu--open');
+//     navToggle.classList.remove('button__menu--open');
+//   }
+// });
