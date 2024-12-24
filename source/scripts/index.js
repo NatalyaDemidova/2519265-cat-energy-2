04113605cfdd0a5665062645ca8b1a1cb7a158be
= <demidovanatalia691991@gmail.com>
2024-12-24 16:57:59 +0300

    module7-task1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,20 +1,20 @@
 /* в этот файл добавляет скрипты*/
 const navMain = document.querySelector('.nav-menu');
-const navToggle = document.querySelector('.button__menu');
+const navToggle = document.querySelector('.button-menu');
 
 navMain.classList.remove('nav-menu--nojs');
-navToggle.classList.remove('button__menu--nojs');
+navToggle.classList.remove('button-menu--nojs');
 
-// navToggle.addEventListener('click', () => {
-//   if (navMain.classList.contains('nav-menu--closed')) {
-//     navMain.classList.remove('nav-menu--closed');
-//     navToggle.classList.remove('button__menu--closed');
-//     navMain.classList.add('nav-menu--open');
-//     navToggle.classList.add('button__menu--open');
-//   } else {
-//     navMain.classList.add('nav-menu--closed');
-//     navToggle.classList.add('button__menu--closed');
-//     navMain.classList.remove('nav-menu--open');
-//     navToggle.classList.remove('button__menu--open');
-//   }
-// });
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('nav-menu--closed')) {
+    navMain.classList.remove('nav-menu--closed');
+    navToggle.classList.remove('button-menu--closed');
+    navMain.classList.add('nav-menu--open');
+    navToggle.classList.add('button-menu--open');
+  } else {
+    navMain.classList.add('nav-menu--closed');
+    navToggle.classList.add('button-menu--closed');
+    navMain.classList.remove('nav-menu--open');
+    navToggle.classList.remove('button-menu--open');
+  }
+});
